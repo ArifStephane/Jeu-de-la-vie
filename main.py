8a9cca67761f6f5dd4a7a85fe1534bb28caecb2f
Fidel HOUNKPATIN <fidelhounkpatin@MacBook-Pro-de-Fidel.local>
2025-10-24 17:01:41 +0200

    fix notation

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ frame = np.array([
     [0, 0, 0, 0, 0, 0, 0]
 ])
 
-def compute_number_of_neighbors(paded_frame, index_row, index_column):
-    neighborhood = paded_frame[index_row:index_row+3, index_column:index_column+3]
-    return np.sum(neighborhood) - paded_frame[index_row+1, index_column+1]
+def compute_number_of_neighbors(padded_frame, index_row, index_column):
+    neighborhood = padded_frame[index_row:index_row+3, index_column:index_column+3]
+    return np.sum(neighborhood) - padded_frame[index_row+1, index_column+1]
 
 def compute_next_frame(frame):
-    paded_frame = np.pad(frame, 1, mode='constant')
+    padded_frame = np.pad(frame, 1, mode='constant')
     new_frame = np.zeros_like(frame)
-    for i in range(frame.shape[0]):
-        for j in range(frame.shape[1]):
-            neighbors = compute_number_of_neighbors(paded_frame, i, j)
-            if frame[i, j] == 1:
+    for row in range(frame.shape[0]):
+        for col in range(frame.shape[1]):
+            neighbors = compute_number_of_neighbors(padded_frame, row, col)
+            if frame[row, col] == 1:
                 if neighbors == 2 or neighbors == 3:
-                    new_frame[i, j] = 1
+                    new_frame[row, col] = 1
                 else:
-                    new_frame[i, j] = 0
+                    new_frame[row, col] = 0
             else:
                 if neighbors == 3:
-                    new_frame[i, j] = 1
+                    new_frame[row, col] = 1
                 else:
-                    new_frame[i, j] = 0
+                    new_frame[row, col] = 0
     return new_frame
 
 while True:
@@ -39,4 +39,3 @@ while True:
     time.sleep(1)
     os.system('cls' if os.name == 'nt' else 'clear')
     frame = compute_next_frame(frame)
-
